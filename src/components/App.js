59065baf17578c3ecd21015cc65eaab9aa1514a5
Siddharth Hota <siddharthhota26@gmail.com>
2023-10-16 13:50:08 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,46 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [time, setTime] = useState(0);
+  const [isActive, setIsActive] = useState(true);
+  const [loadTime, setLoadTime] = useState(Date.now());
+
+  useEffect(()=>{
+    let interval;
+    interval = setInterval(()=>{
+      if(isActive){
+        console.log(loadTime, time);
+        setTime(Date.now() - loadTime);
+      }
+    })
+
+    return(()=>{
+      clearInterval(interval);
+      }
+    )
+  }, [isActive, loadTime])
+
+  useEffect(()=>{
+    function handleDocVisibility(){
+      setIsActive(!document.hidden);
+      if(!document.hidden){
+        setLoadTime(Date.now()-time);
+      }
+    }
+
+    document.addEventListener('visibilitychange', handleDocVisibility);
+
+    return(()=>{
+      document.removeEventListener('visibilitychange', handleDocVisibility);
+    })
+  })
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>You've been on this page for {(time/1000).toFixed(0)} seconds.</p>
     </div>
   )
 }
